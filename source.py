453f3b2ee325a159b601e7e8269da35fe93397dc
Daud Ahmad <73556387+DaudAhmad0303@users.noreply.github.com>
2022-11-17 14:11:29 +0500

    coded for accessing parameters of JSON

--- source.py
+++ source.py
@@ -37,8 +37,9 @@ with open("DatasetGovProc.json", encoding='UTF-8') as file:
 # # New Document ('DatasetGovProc.json') based Extracting
 # For Searching the procedure name in JSON
 # for i in range(len(data['data'][0])):
-#     proc_name = data["data"][i]["subThematics"][0]["govprocedure"][0]["title"]
-#     print("Procedure Name:", str(proc_name).capitalize())
+#     if "title" in data["data"][i]["subThematics"][0]["govprocedure"][0]:
+#         proc_name = data["data"][i]["subThematics"][0]["govprocedure"][0]["title"]
+#         print("Procedure Name:", str(proc_name).capitalize())
 
 # # For Searching the procedure Description in JSON
 # for i in range(len(data['data'][0])):
@@ -48,5 +49,30 @@ with open("DatasetGovProc.json", encoding='UTF-8') as file:
 
 # # To print the receiving administration name
 # for i in range(len(data['data'][0])):
-#     receivingAdministrationName = data["data"][i]["subThematics"][0]["govprocedure"][0]["details"]["receivingAdministrations"][0]["title"]
-#     print("Procedure Receiving Administration:", receivingAdministrationName)
+#     if "title" in data["data"][i]["subThematics"][0]["govprocedure"][0]["details"]["receivingAdministrations"][0]:
+#         receivingAdministrationName = data["data"][i]["subThematics"][0]["govprocedure"][0]["details"]["receivingAdministrations"][0]["title"]
+#         print("Procedure Receiving Administration:", receivingAdministrationName)
+
+# # To print the delay in service
+# for i in range(len(data['data'][0])):
+#     if "delay" in data["data"][i]["subThematics"][0]["govprocedure"][0]["details"]:
+#         timeDelayed = data["data"][i]["subThematics"][0]["govprocedure"][0]["details"]["delay"]
+#         print("Time Taken for Procedure:", timeDelayed)
+
+# # To print the price to pay for a Procedure
+# for i in range(len(data['data'][0])):
+#     if "price" in data["data"][i]["subThematics"][0]["govprocedure"][0]["details"]:
+#         priceForProcedure = data["data"][i]["subThematics"][0]["govprocedure"][0]["details"]["price"]
+#         print("Money to Pay for Procedure:", priceForProcedure)
+
+# # To print the Documents required for a procedure
+# for i in range(len(data['data'][0])):
+#     if "documents" in data["data"][i]["subThematics"][0]["govprocedure"][0]["details"]:
+#         listOfDocuments = data["data"][i]["subThematics"][0]["govprocedure"][0]["details"]["documents"]
+#         documents = list()
+#         for i in range(len(listOfDocuments)):
+#             documents.append(str(listOfDocuments[i]["title"]).capitalize())
+#         print("Following documents are required for the specified procedure:")
+#         for i in range(1, len(documents)+1):
+#             print(f"{i}. {documents[i-1]}.")
+#         print()
